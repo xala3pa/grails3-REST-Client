ca9d635db948d544915435020a2241332249a729
xala3pa <salazar3pa@gmail.com>
2016-12-09 18:57:24 +0100

    add hystrix dashboard support

--- grails-app/init/restclient/Application.groovy
+++ grails-app/init/restclient/Application.groovy
@@ -2,7 +2,11 @@ package restclient
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.context.annotation.Configuration
 
+@Configuration
+@EnableHystrixDashboard
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
